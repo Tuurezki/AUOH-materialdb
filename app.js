37567cb8f1a3e99324dc8d47d7a9c1e9f874301c
Tuure Takala <tuure.takala@seamk.fi>
2020-04-03 19:45:49 +0300

    Commit database connected

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
+const mongoose = require('mongoose');
 const app = express();
 const body_parser = require('body-parser');
 
@@ -24,12 +25,30 @@ app.post("/api/material", material_controller.api_post_material);
 
 // READ
 app.get("/api/materials", material_controller.api_get_materials);
+
 // UPDATE
 
 // DELETE
 
+const database_uri ="mongodb+srv://server:f1IFdTNtzuvzqZQS@cluster0-wfba5.mongodb.net/test?retryWrites=true&w=majority"
 
-app.listen(port);
+// Mongo db connection
+mongoose.connect(database_uri, {
+    useCreateIndex: true, 
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log('Database connected');
+    app.listen(port);
+}).catch(err => {
+    console.log(err);
+});
 
 
+// f1IFdTNtzuvzqZQS
+// server
+// mongodb+srv://server:<password>@cluster0-wfba5.mongodb.net/test?retryWrites=true&w=majority
+// npm install mongoose
+// npm install express
 
