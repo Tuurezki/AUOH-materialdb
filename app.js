4e9b2daf59dfc1a38ddac89cecb336ea9c26ee80
Tuure Takala <tuure.takala@seamk.fi>
2020-04-03 19:11:35 +0300

    commit 2

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const port = process.env.PORT || 8080;
 const app = express();
 const body_parser = require('body-parser');
 
+const material_controller = require('./material_controller');
+
 app.use(body_parser.json()); //req.body.name
 app.use(body_parser.urlencoded({
     extended:true
@@ -10,7 +12,21 @@ app.use(body_parser.urlencoded({
 
 app.use( (req, res, next)=>{
     console.log(req.method, ' ', req.path);
+    next();
 }); //GET /api/materials
+
+
+// RESTful API
+// CRUD OPERATIONS
+
+// CREATE
+app.post("/api/material", material_controller.api_post_material);
+
+// READ
+app.get("/api/materials", material_controller.api_get_materials);
+// UPDATE
+
+// DELETE
 
 
 app.listen(port);
